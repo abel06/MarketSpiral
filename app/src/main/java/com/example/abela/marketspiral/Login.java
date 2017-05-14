3ae1623386638728bee09d938fe4c7962c4342d0
Abel <abelmesfin2003@gmail.com>
2017-05-13 18:27:59 -0700

    no change

--- app/src/main/java/com/example/abela/marketspiral/Login.java
+++ app/src/main/java/com/example/abela/marketspiral/Login.java
@@ -1,32 +1,98 @@
 package com.example.abela.marketspiral;
 
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.abela.marketspiral.Core.RemoteTask;
+import com.example.abela.marketspiral.Core.SearchActivity;
+import com.example.abela.marketspiral.Google.PlayServiceCheck;
 import com.example.abela.marketspiral.Utility.Actions;
 import com.example.abela.marketspiral.interfaces.RemoteResponse;
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.plus.Plus;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
-public class Login extends AppCompatActivity implements RemoteResponse {
+public class Login extends AppCompatActivity implements RemoteResponse, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+    private static final int RC_SIGN_IN = 0;
     Context mContext;
+
+
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    private CallbackManager callbackManager;
+    private TwitterLoginButton twitterLoginButton;
+    GoogleApiClient mGoogleApiClient;
+
+
+    //Facebook login button
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Profile profile = Profile.getCurrentProfile();
+            nextActivity();
+        }
+        @Override
+        public void onCancel() {        }
+        @Override
+        public void onError(FacebookException e) {      }
+    };
+    private PendingIntent activityDetectionPendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        FloatingActionButton fabSignup= (FloatingActionButton) findViewById(R.id.fab_signup);
+   FloatingActionButton fabSignup= (FloatingActionButton) findViewById(R.id.fab_signup);
         fabSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,7 +114,171 @@ public class Login extends AppCompatActivity implements RemoteResponse {
                     login(data);
                 }
             });
-    mContext.getApplicationContext();
+    mContext=getApplicationContext();
+
+
+        //----------------------------------------------------------
+
+      //---------------------------------------------------------------------------
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                nextActivity();
+            }
+        };
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginBtn.setCompoundDrawablesWithIntrinsicBounds(0,0,0,0);
+        callback = new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                nextActivity();
+                Toast.makeText(getApplicationContext(), "Logging in...", Toast.LENGTH_SHORT).show();    }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Loggin canceled...", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(getApplicationContext(), "Sorry unable to login...", Toast.LENGTH_SHORT).show();
+            }
+        };
+        loginButton.setReadPermissions("user_friends");
+        loginButton.registerCallback(callbackManager, callback);
+      //----------------------------------------------------------------------
+        twitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                nextActivity();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+//==============================================================================
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        //------------------------------------------------------------------
+        PlayServiceCheck playServiceCheck = new PlayServiceCheck(getApplicationContext());
+        if (playServiceCheck.isPlayServiceOk()) {
+            buildGoogleApiClient();
+            mGoogleApiClient.connect();
+        }
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mGoogleApiClient!=null){
+                    try{
+                        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                        startActivityForResult(signInIntent,RC_SIGN_IN);
+                    }catch (Exception e){
+                        Log.d("ab",""+e);
+                    }
+
+                    //
+                }
+            }
+        });
+    }
+    synchronized void buildGoogleApiClient() {
+
+        try{
+            GoogleSignInOptions gso = new GoogleSignInOptions.
+                    Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+                    requestScopes(new Scope(Scopes.PLUS_LOGIN)).
+                    requestEmail().
+                    build();
+
+            mGoogleApiClient = new GoogleApiClient.Builder(this).
+                    enableAutoManage(this, this).
+                    addApi(Auth.GOOGLE_SIGN_IN_API, gso).
+                    addApi(Plus.API).
+                    build();
+        }catch (Exception e){
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Facebook login
+        Profile profile = Profile.getCurrentProfile();
+       // nextActivity();
+    }
+    private void nextActivity(){
+        //if(profile != null){
+            Intent search = new Intent(Login.this, SearchActivity.class);
+           /* main.putExtra("name", profile.getFirstName());
+            main.putExtra("surname", profile.getLastName());
+            main.putExtra("imageUrl", profile.getProfilePictureUri(200,200).toString());*/
+            startActivity(search);
+       // }
+    }
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        //Facebook login
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        super.onActivityResult(requestCode, responseCode, intent);
+        //Facebook login
+        callbackManager.onActivityResult(requestCode, responseCode, intent);
+        Log.d("ab","result "+requestCode);
+        //twitter Login
+        twitterLoginButton.onActivityResult(requestCode, responseCode, intent);
+        //google Login
+        if (requestCode == RC_SIGN_IN) {
+            try {
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(intent);
+                Log.d("ab","result "+result);
+            if (result.isSuccess()) {
+                GoogleSignInAccount acct = result.getSignInAccount();
+
+                // Get account information
+                String mFullName = acct.getDisplayName();
+                String mEmail = acct.getEmail();
+                nextActivity();
+            }
+    }catch (Exception e){
+                Log.d("ab","result "+e);
+            }
+        }
+
     }
     public void login(HashMap<String,String> data) {
 
@@ -86,6 +316,22 @@ public class Login extends AppCompatActivity implements RemoteResponse {
 
     @Override
     public void searchItem(int id) {
+
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
 
     }
 
