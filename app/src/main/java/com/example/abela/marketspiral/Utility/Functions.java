7b68eed5cbcf775d44adb8eed72dc28eef5eb972
Marco Anastasio <Marco_Anastasio@libero.it>
2017-05-18 11:35:50 +0200

    Fixed when dealing with empty values (Prevent PHP weird parse error)

--- app/src/main/java/com/example/abela/marketspiral/Utility/Functions.java
+++ app/src/main/java/com/example/abela/marketspiral/Utility/Functions.java
@@ -14,15 +14,23 @@ public final class Functions {
 
         for (String key: data.keySet()) {
 
-            if(result.isEmpty()) {
-                result += key + "=" +data.get(key);
-            }else{
-                result +="&"+key+"="+data.get(key);
+            String value = data.get(key);
+            if(!value.isEmpty()) {
+                if (result.isEmpty()) {
+                    result += key + "=" + data.get(key);
+                } else {
+                    result += "&" + key + "=" + data.get(key);
+                }
             }
 
         }
 
         return result;
+    }
+
+
+    public static HashMap<String,String> RetrieveDataFromUI(){
+        return null;
     }
 
 }
