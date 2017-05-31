4d4bb5fbb2d105ed0a7cb850c22d446bbd7c6ae7
Marco Anastasio <Marco_Anastasio@libero.it>
2017-06-01 01:14:15 +0200

    Removed unused functions

--- app/src/main/java/com/example/abela/marketspiral/test_activity.java
+++ app/src/main/java/com/example/abela/marketspiral/test_activity.java
@@ -2,29 +2,15 @@ package com.example.abela.marketspiral;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
-import android.support.annotation.VisibleForTesting;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
-
-import com.akexorcist.googledirection.model.Line;
 import com.example.abela.marketspiral.Utility.ImageInfo;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class test_activity extends AppCompatActivity {
 
@@ -32,7 +18,6 @@ public class test_activity extends AppCompatActivity {
     private GridView gridView;
     private CustomImageAdapter adapter;
     static final int PICK_IMAGE_REQUEST = 1;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,43 +59,6 @@ public class test_activity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
     }
-
-
-    private Bitmap decodeSampledBitmapFromUri(String path, int reqWidth, int reqHeight) {
-
-        Bitmap bm = null;
-        // First decode with inJustDecodeBounds=true to check dimensions
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeFile(path, options);
-
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        bm = BitmapFactory.decodeFile(path, options);
-
-        return bm;
-    }
-
-    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
-            // Raw height and width of image
-            final int height = options.outHeight;
-            final int width = options.outWidth;
-            int inSampleSize = 1;
-
-            if (height > reqHeight || width > reqWidth) {
-                if (width > height) {
-                    inSampleSize = Math.round((float)height / (float)reqHeight);
-                } else {
-                    inSampleSize = Math.round((float)width / (float)reqWidth);
-                }
-            }
-
-            return inSampleSize;
-    }
-
 
     public void browseImage(View view) {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
