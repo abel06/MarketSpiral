1141f5fe4fcb670fb95c13a76a430142b81c54ba
Marco Anastasio <Marco_Anastasio@libero.it>
2017-06-01 13:14:29 +0200

    Added comments

--- app/src/main/java/com/example/abela/marketspiral/CustomImageAdapter.java
+++ app/src/main/java/com/example/abela/marketspiral/CustomImageAdapter.java
@@ -1,9 +1,7 @@
 package com.example.abela.marketspiral;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +12,19 @@ import java.util.ArrayList;
 
 /**
  * Created by HaZe on 5/31/17.
+ *
  */
 
+/**This image implements a base adapter for our image grid*/
 public class CustomImageAdapter extends BaseAdapter {
 
+    /**Array of imageinfo used to store information about image data*/
     private ArrayList<ImageInfo> images_info;
+
     private Context context;
+
+    /**Index of last rendered image needed to prevent performance issues*/
+    private int last_index = 0;
 
     public CustomImageAdapter (Context context, ArrayList<ImageInfo> images_info){
 
@@ -48,6 +53,8 @@ public class CustomImageAdapter extends BaseAdapter {
     }
 
 
+
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 
@@ -55,10 +62,7 @@ public class CustomImageAdapter extends BaseAdapter {
 
         View gridView;
 
-//        if (view == null) {
-
-            gridView = new View(context);
-
+        if (view == null) {
             // get layout from mobile.xml
             gridView = inflater.inflate(R.layout.grid_item, null);
 
@@ -66,14 +70,28 @@ public class CustomImageAdapter extends BaseAdapter {
             Bitmap bt = decodeSampledBitmapFromUri(images_info.get(i).getPath(), 200, 200);
 
             imageView.setImageBitmap(bt);
-//        }else{
-//            gridView = view;
-//        }
+            last_index = i;
+        }else{
+
+            /**In case the grid have already been populated in case of a refresh or a value inside the input text the images don't have to be
+             * reload inside the grid, in case a new image is added this HAVE to be rendered*/
+            gridView = view;
+
+            //TODO fix perforance issue when scrolling and using keyboard (while editing text)
+            if(i > last_index) {
+            ImageView imageView = (ImageView) gridView.findViewById(R.id.image_grid_item);
+            Bitmap bt = decodeSampledBitmapFromUri(images_info.get(i).getPath(), 200, 200);
+            imageView.setImageBitmap(bt);
+                last_index = i;
+            }
+        }
 
         return gridView;
     }
 
 
+
+/**This function is used to resize the images*/
     private Bitmap decodeSampledBitmapFromUri(String path, int reqWidth, int reqHeight) {
 
         Bitmap bm = null;
@@ -91,6 +109,9 @@ public class CustomImageAdapter extends BaseAdapter {
 
         return bm;
     }
+
+    /**This is a utility function used to mantain the same aspect ratio of the original image*/
+    //TODO Insert this function inside utility package
 
     private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         // Raw height and width of image
