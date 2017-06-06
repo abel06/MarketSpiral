87c2e267e0beffdba90318cf9cd09ed226de4908
Marco Anastasio <Marco_Anastasio@libero.it>
2017-06-06 12:55:25 +0200

    Updates the location for the new item

--- app/src/main/java/com/example/abela/marketspiral/ADD/AddLocation.java
+++ app/src/main/java/com/example/abela/marketspiral/ADD/AddLocation.java
@@ -8,25 +8,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
-
-import com.daimajia.slider.library.SliderLayout;
+import com.example.abela.marketspiral.Core.RemoteTask;
 import com.example.abela.marketspiral.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.example.abela.marketspiral.Utility.Actions;
+import com.example.abela.marketspiral.interfaces.RemoteResponse;
+import com.example.abela.marketspiral.test_activity;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class AddLocation extends AppCompatActivity implements OnMapReadyCallback {
+import java.util.HashMap;
+
+public class AddLocation extends AppCompatActivity implements OnMapReadyCallback, RemoteResponse {
     private GoogleMap mMap;
     public static SupportMapFragment mapFragment;
+    private int item_id;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_location);
+
+        Bundle extras = getIntent().getExtras();
+
+        item_id = (int) extras.get("id");
 
         //-----------------------------------------------
         FragmentManager fm = getSupportFragmentManager();
@@ -49,11 +57,11 @@ public class AddLocation extends AppCompatActivity implements OnMapReadyCallback
                   latitude=marker.getPosition().latitude;
                   longitude=marker.getPosition().longitude;
                    // getIntent().getExtras()
-                    Intent add_image=new Intent(AddLocation.this,AddImage.class);
-                    add_image.putExtra("lat",latitude);
-                    add_image.putExtra("lng",longitude);
-                    add_image.putExtras(getIntent().getExtras());
-                    AddLocation.this.startActivity(add_image);
+                    HashMap<String,String> data = new HashMap<String, String>();
+                    data.put("lat",String.valueOf(latitude));
+                    data.put("long",String.valueOf(longitude));
+                    data.put("id",String.valueOf(item_id));
+                    new RemoteTask(Actions.ADD_LOCATION,data,AddLocation.this,false).execute();
                 }
                else {
                     Toast.makeText(getApplicationContext(),"Please put marker on your home location",Toast.LENGTH_SHORT).show();
@@ -78,6 +86,66 @@ public class AddLocation extends AppCompatActivity implements OnMapReadyCallback
                 }
             }
         });
+
+    }
+
+    @Override
+    public void loginFinished(int id) {
+
+    }
+
+    @Override
+    public void registerFinished(int value, boolean externalService) {
+
+    }
+
+    @Override
+    public void searchFinished(int value, Object result) {
+
+    }
+
+    @Override
+    public void geocodeFinished(int id, Object result) {
+
+    }
+
+    @Override
+    public void itemAdded(int id) {
+
+    }
+
+    @Override
+    public void itemRemoved(int id) {
+
+    }
+
+    @Override
+    public void searchItem(int id) {
+
+    }
+
+    @Override
+    public void addLocation(int return_state) {
+
+        if(return_state == 0){
+        Intent i = new Intent(this, test_activity.class);
+            i.putExtra("id",item_id);
+            startActivity(i);
+
+
+        }else {
+            Toast.makeText(getApplicationContext(),"Could not add/upload location",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void addItem(int id) {
+
+    }
+
+    @Override
+    public void imageUploaded(int value) {
 
     }
 }
