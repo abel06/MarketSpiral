35b6db955caf223cf373b6bff3a69a3e3d878cb6
Marco Anastasio <Marco_Anastasio@libero.it>
2017-06-08 11:24:54 +0200

    Finished Registor class

--- app/src/main/java/com/example/abela/marketspiral/Registor.java
+++ app/src/main/java/com/example/abela/marketspiral/Registor.java
@@ -2,11 +2,20 @@ package com.example.abela.marketspiral;
 
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Registor extends AppCompatActivity {
+import com.example.abela.marketspiral.Core.RemoteTask;
+import com.example.abela.marketspiral.Utility.Actions;
+import com.example.abela.marketspiral.interfaces.RemoteResponse;
+
+import java.util.HashMap;
+
+public class Registor extends AppCompatActivity implements RemoteResponse {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,81 @@ public class Registor extends AppCompatActivity {
                 Registor.this.startActivity(login);
             }
         });
+
+        findViewById(R.id.signup_layout).findViewById(R.id.btn_signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                register();
+            }
+        });
+
+    }
+
+    public void register() {
+
+
+        String username = ((TextInputEditText)findViewById(R.id.signup_layout).findViewById(R.id.input_username)).getText().toString();
+        String password= ((TextInputEditText)findViewById(R.id.signup_layout).findViewById(R.id.input_password)).getText().toString();
+        String email = ((TextInputEditText)findViewById(R.id.signup_layout).findViewById(R.id.input_email)).getText().toString();
+
+        HashMap<String,String> data = new HashMap<>(3);
+        data.put("username",username);
+        data.put("password",password);
+        data.put("full_name",email);
+
+        new RemoteTask(Actions.USER_REGISTRATION,data,this,false).execute();
+
+    }
+
+    @Override
+    public void loginFinished(int id) {
+
+    }
+
+    @Override
+    public void registerFinished(int value, boolean externalService) {
+
+        Toast.makeText(getApplicationContext(),"Id :" + value, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void searchFinished(int value, Object result) {
+
+    }
+
+    @Override
+    public void geocodeFinished(int id, Object result) {
+
+    }
+
+    @Override
+    public void addItem(int id) {
+
+    }
+
+    @Override
+    public void itemAdded(int id) {
+
+    }
+
+    @Override
+    public void itemRemoved(int id) {
+
+    }
+
+    @Override
+    public void searchItem(int id) {
+
+    }
+
+    @Override
+    public void addLocation(int return_state) {
+
+    }
+
+    @Override
+    public void imageUploaded(int value) {
+
     }
 }
